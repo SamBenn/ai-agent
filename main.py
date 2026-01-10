0b2e6be78cf0c39cb6d78cfa5cc28ccf022ee0bf
SamBenn <sam-benn@hotmail.co.uk>
2026-01-10 20:47:19 +0000

    first tool

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from prompts import system_prompt
+
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
@@ -15,13 +17,39 @@ args = parser.parse_args()
 
 def main():
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
+
+    schema_get_files_info = types.FunctionDeclaration(
+        name="get_files_info",
+        description="Lists files in a specified directory relative to the working directory, providing file size and directory status",
+        parameters=types.Schema(
+            type=types.Type.OBJECT,
+            properties={
+                "directory": types.Schema(
+                    type=types.Type.STRING,
+                    description="Directory path to list files from, relative to the working directory (default is the working directory itself)",
+                ),
+            },
+        ),
+    )
+
+    available_functions = types.Tool(
+        function_declarations=[schema_get_files_info],
+    )
+
+    config = types.GenerateContentConfig(system_instruction=system_prompt, temperature=0, 
+                                         tools=[available_functions])
+
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=messages, 
+                                              config=config)
     if args.verbose:
         print(f'User prompt: {args.user_prompt}')
         print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
         print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
     
     print(response.text)
+    if response.function_calls != None:
+        for function_call in response.function_calls:
+            print(f"Calling function: {function_call.name}({function_call.args})")
 
 if __name__ == "__main__":
     main()
